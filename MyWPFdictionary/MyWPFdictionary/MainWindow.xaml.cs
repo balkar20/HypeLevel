46371995d9be8038b9494f69b411a09ed538c05d
vbalkarov <balkar20@mail.ru>
2019-02-04 15:38:53 +0300

    kjkj

--- MyWPFdictionary/MyWPFdictionary/MainWindow.xaml.cs
+++ MyWPFdictionary/MyWPFdictionary/MainWindow.xaml.cs
@@ -1,13 +1,8 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.ComponentModel;
 using System.Diagnostics;
-using System.Linq;
 using System.Windows;
-using System.Windows.Forms;
-using MyWPFdictionary.dataBase;
 using MyWPFdictionary.Helpers;
-using MyWPFdictionary.Models;
 using KeyEventArgs = System.Windows.Input.KeyEventArgs;
 using MessageBox = System.Windows.MessageBox;
 using TextBox = System.Windows.Controls.TextBox;
--- MyWPFdictionary/MyWPFdictionary/WordRepository.cs
+++ MyWPFdictionary/MyWPFdictionary/WordRepository.cs
@@ -1,11 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.IO;
-using System.Linq;
 using System.Text.RegularExpressions;
-using MyWPFdictionary.dataBase;
 using MyWPFdictionary.Helpers;
-using MyWPFdictionary.Models;
 
 namespace MyWPFdictionary
 {
