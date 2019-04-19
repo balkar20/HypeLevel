98aedc821027e82112b7fd6a7782bce4bd3e0f64
balkar20 <balkar20@mail.ru>
2019-04-19 17:22:53 +0300

    ррр

---
+++ CrackInfo/CrackInfo/Pages/Factorial.cshtml.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace CrackInfo.Pages
+{
+    public class FactorialModel : PageModel
+    {
+        public int Number { get; set; }
+        public int Result { get; set; }
+        public bool IsCorrect { get; set; } = true;
+
+        public void OnGet(int? number)
+        {
+            if (number == null || number < 1)
+            {
+                IsCorrect = false;
+                return;
+            }
+
+            Number = number.Value;
+            Result = 1;
+            for (int i = 1; i <= number; i++)
+            {
+                Result *= i;
+            }
+        }
+
+    }
+}
