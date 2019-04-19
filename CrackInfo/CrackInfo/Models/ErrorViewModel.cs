aab2ffa837ccdf20f9aa9078e7220df46dcd263b
balkar20 <balkar20@mail.ru>
2019-04-19 15:04:31 +0300

    hhh

---
+++ CrackInfo/CrackInfo/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace CrackInfo.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
