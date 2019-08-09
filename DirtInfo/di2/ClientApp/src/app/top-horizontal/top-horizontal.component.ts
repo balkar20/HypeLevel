ed1be31cdd52bbdaae1aca9b7705db09ff755d22
balkar20 <balkar20@mail.ru>
2019-08-09 16:25:35 +0300

    jkjk

--- DirtInfo/di2/ClientApp/src/app/top-horizontal/top-horizontal.component.ts
+++ DirtInfo/di2/ClientApp/src/app/top-horizontal/top-horizontal.component.ts
@@ -25,19 +25,19 @@ export class TopHorizontalComponent implements OnInit {
 
   trackByItems(index: number, item: NewsRebate): number { return item.id; }
 
-  setLinkStyle(id:number) :any{
+  setLinkStyle(path) :any{
+    console.log(`passed path is: ${path}`);
     // console.log(id);
     // console.log(this.newsRebates.find(item => item.id == id).id);
     // console.log(id == this.newsRebates.find(item => item.id == id).id);
     // console.log("-----------------------------------------");
     // console.log(`Now i lo you path: ${this.newsRebates.find(item => item.id == id).imgPath}`)
-    var obj: NewsRebate = this.newsRebates.find(item => item.id == id) as NewsRebate;
-    var url : number = obj.id;
-    console.log(`imagepath is : ${url}`);
+    // var obj: NewsRebate = this.newsRebates.find(item => item.id == id) as NewsRebate;
+    // var url : number = obj.id;
+    // console.log(`imagepath is : ${url}`);
     // return this.sanitizer.bypassSecurityTrustStyle(`url(${url})`);
-    let path = "https://cs8.pikabu.ru/avatars/2169/x2169459-1857383472.png"
+    // let path = "https://cs8.pikabu.ru/avatars/2169/x2169459-1857383472.png"
     return {
-
       'background-image': `url(\'${path}\')`,
       'display': 'block',
       'color': '#337ab7',
@@ -45,6 +45,9 @@ export class TopHorizontalComponent implements OnInit {
       'width': '100%',
       'height': '100%',
       'position': 'relative',
+      'border-radius': '30%',
+      'font-family':'fantasy',
+      'font-size':'large',
     }
   }
 }
